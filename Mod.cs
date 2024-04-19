37c0fa6e4aafa1c72982a88142afa83e2beb5957
F_thx <1764362678@qq.com>
2024-04-19 15:21:02 +0800

    Initial commit

---
+++ Mod.cs
@@ -0,0 +1,22 @@
+﻿using Colossal.Logging;
+using Game;
+using Game.Modding;
+using Game.SceneFlow;
+
+namespace CustomAssetPack
+{
+    public class Mod : IMod
+    {
+        public static ILog log = LogManager.GetLogger($"{nameof(CustomAssetPack)}.{nameof(Mod)}")
+            .SetShowsErrorsInUI(false);
+
+        public void OnLoad(UpdateSystem updateSystem)
+        {
+            log.Info("Loading Asset Pack " + nameof(CustomAssetPack));
+        }
+
+        public void OnDispose()
+        {
+        }
+    }
+}
